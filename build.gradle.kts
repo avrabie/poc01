d62584cde6cd455f955094de4e907e11c0aa6043
avrabie <mr.vrabie@gmail.com>
2025-03-26 14:16:26 +0100

    adding ai

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
 }
+val springAiVersion by extra("1.0.0-M6")
 
 group = "com.execodex"
 version = "0.0.1-SNAPSHOT"
@@ -26,13 +27,19 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.apache.pdfbox:pdfbox:2.0.27") // or latest version
-    implementation("org.apache.opennlp:opennlp-tools:1.9.4") // or latest version
+    implementation("org.apache.opennlp:opennlp-tools:1.9.4")
+    implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter") // or latest version
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.ai:spring-ai-bom:$springAiVersion")
+    }
 }
 
 tasks.withType<Test> {
--- src/main/java/com/execodex/poc01/controller/IakaController.java
+++ src/main/java/com/execodex/poc01/controller/IakaController.java
@@ -1,13 +1,36 @@
 package com.execodex.poc01.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @RestController
 public class IakaController {
+
+    private final ChatClient.Builder chatClientBuilder;
+    private final ChatClient chatClient;
+
+    public IakaController(ChatClient.Builder chatClientBuilder) {
+        this.chatClientBuilder = chatClientBuilder;
+        chatClient = chatClientBuilder.build();
+    }
+
     @GetMapping("/iaka")
     public Mono<String> iaka() {
-        return Mono.just("Iaka");
+        ChatClient.ChatClientRequestSpec prompt = chatClient.prompt("Tell me a software engineering joke");
+
+        Mono<String> stringMono = Mono.fromCallable(() -> {
+            ChatClient.ChatClientRequestSpec requestSpec = chatClient.prompt("Tell me a software engineering joke");
+            ChatClient.CallResponseSpec call = requestSpec.call();
+            return call.content();
+        }).subscribeOn(Schedulers.boundedElastic());
+        return stringMono;
+    }
+
+    @GetMapping("/hello")
+    public Mono<String> hello() {
+        return Mono.just("Hello World!");
     }
 }
